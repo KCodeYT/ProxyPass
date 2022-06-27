07f713bf7e9c863789d5f27d0630ac19765e20b8
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2022-06-27 23:27:55 +0100

    Use block states for block items

--- src/main/java/com/nukkitx/proxypass/network/bedrock/util/RecipeUtils.java
+++ src/main/java/com/nukkitx/proxypass/network/bedrock/util/RecipeUtils.java
@@ -1,8 +1,10 @@
 package com.nukkitx.proxypass.network.bedrock.util;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.nukkitx.nbt.NBTOutputStream;
 import com.nukkitx.nbt.NbtMap;
+import com.nukkitx.nbt.NbtType;
 import com.nukkitx.nbt.NbtUtils;
 import com.nukkitx.protocol.bedrock.data.inventory.*;
 import com.nukkitx.protocol.bedrock.packet.CraftingDataPacket;
@@ -22,6 +24,16 @@ public class RecipeUtils {
     private static final char[] SHAPE_CHARS = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
     public static void writeRecipes(CraftingDataPacket packet, ProxyPass proxy) {
+        // Load up block palette for conversion, if we can find it.
+        Object object = proxy.loadGzipNBT("block_palette.nbt");
+        List<NbtMap> paletteTags = null;
+        if (object instanceof NbtMap) {
+            NbtMap map = (NbtMap) object;
+            paletteTags = map.getList("blocks", NbtType.COMPOUND);
+        }
+
+        List<NbtMap> recipePalette = new ArrayList<>();
+
         List<CraftingDataEntry> entries = new ArrayList<>();
         List<PotionMixDataEntry> potions = new ArrayList<>();
         List<ContainerMixDataEntry> containers = new ArrayList<>();
@@ -41,7 +53,7 @@ public class RecipeUtils {
             if (type == CraftingDataType.SHAPED || type == CraftingDataType.SHAPELESS || type == CraftingDataType.SHAPELESS_CHEMISTRY || type == CraftingDataType.SHULKER_BOX || type == CraftingDataType.SHAPED_CHEMISTRY) {
                 entry.id = craftingData.getRecipeId();
                 entry.priority = craftingData.getPriority();
-                entry.output = writeItemArray(craftingData.getOutputs().toArray(new ItemData[0]), true);
+                entry.output = writeItemArray(paletteTags, recipePalette, craftingData.getOutputs().toArray(new ItemData[0]), true);
             }
             if (type == CraftingDataType.SHAPED || type == CraftingDataType.SHAPED_CHEMISTRY) {
 
@@ -56,7 +68,7 @@ public class RecipeUtils {
                     int index = height * craftingData.getWidth();
                     for (int width = 0; width < craftingData.getWidth(); width++) {
                         int slot = index + width;
-                        Item item = itemFromNetwork(inputs.get(slot), false);
+                        Item item = itemFromNetwork(paletteTags, recipePalette, inputs.get(slot), false);
 
                         if (item == Item.EMPTY) {
                             continue;
@@ -85,15 +97,15 @@ public class RecipeUtils {
                 entry.input = itemMap;
             }
             if (type == CraftingDataType.SHAPELESS || type == CraftingDataType.SHAPELESS_CHEMISTRY || type == CraftingDataType.SHULKER_BOX) {
-                entry.input = writeItemArray(craftingData.getInputs().toArray(new ItemData[0]), false);
+                entry.input = writeItemArray(paletteTags, recipePalette, craftingData.getInputs().toArray(new ItemData[0]), false);
             }
 
             if (type == CraftingDataType.FURNACE || type == CraftingDataType.FURNACE_DATA) {
                 Integer damage = craftingData.getInputDamage();
                 if (damage == 0x7fff) damage = -1;
                 if (damage == 0) damage = null;
-                entry.input = new Item(craftingData.getInputId(), ProxyPass.legacyIdMap.get(craftingData.getInputId()), damage, null, null);
-                entry.output = itemFromNetwork(craftingData.getOutputs().get(0), true);
+                entry.input = new Item(craftingData.getInputId(), ProxyPass.legacyIdMap.get(craftingData.getInputId()), damage, null, null, null);
+                entry.output = itemFromNetwork(paletteTags, recipePalette, craftingData.getOutputs().get(0), true);
             }
             entries.add(entry);
         }
@@ -116,15 +128,21 @@ public class RecipeUtils {
             ));
         }
 
-        Recipes recipes = new Recipes(ProxyPass.CODEC.getProtocolVersion(), entries, potions, containers);
+        // Generate palette
+        ArrayNode paletteNode = ProxyPass.JSON_MAPPER.createArrayNode();
+        for (NbtMap state : recipePalette) {
+            paletteNode.add(nbtToBase64(state));
+        }
+
+        Recipes recipes = new Recipes(ProxyPass.CODEC.getProtocolVersion(), paletteNode, entries, potions, containers);
 
         proxy.saveJson("recipes.json", recipes);
     }
 
-    private static List<Item> writeItemArray(ItemData[] inputs, boolean output) {
+    private static List<Item> writeItemArray(List<NbtMap> palette, List<NbtMap> recipePalette, ItemData[] inputs, boolean output) {
         List<Item> outputs = new ArrayList<>();
         for (ItemData input : inputs) {
-            Item item = itemFromNetwork(input, output);
+            Item item = itemFromNetwork(palette, recipePalette, input, output);
             if (item != Item.EMPTY) {
                 outputs.add(item);
             }
@@ -146,20 +164,35 @@ public class RecipeUtils {
         }
     }
 
-    private static Item itemFromNetwork(ItemData data, boolean output) {
+    private static Item itemFromNetwork(List<NbtMap> palette, List<NbtMap> recipePalette, ItemData data, boolean output) {
         int id = data.getId();
-        String identifier = ProxyPass.legacyIdMap.get(id);
-        Integer damage = (int) data.getDamage();
-        Integer count = data.getCount();
-        String tag = nbtToBase64(data.getTag());
-
         if (id == 0) {
             return Item.EMPTY;
         }
+        String identifier = ProxyPass.legacyIdMap.get(id);
+        Integer damage = data.getDamage();
+        Integer count = data.getCount();
+        String tag = nbtToBase64(data.getTag());
+
+        Integer blockState = null;
+        if (data.getId() < 256) {
+            // If the palette is null, we'll just use the global runtime IDs from this version
+            if (palette == null) {
+                blockState = data.getBlockRuntimeId();
+            } else {
+                NbtMap state = palette.get(data.getBlockRuntimeId());
+                blockState = recipePalette.indexOf(state);
+                if (blockState == -1) {
+                    blockState = recipePalette.size();
+                    recipePalette.add(state);
+                }
+            }
+        }
+
         if (damage == 0 || (damage == -1 && output)) damage = null;
         if (count == 1) count = null;
 
-        return new Item(id, identifier, damage, count, tag);
+        return new Item(id, identifier, damage, count, tag, blockState);
     }
 
     @NoArgsConstructor
@@ -201,18 +234,20 @@ public class RecipeUtils {
     @Value
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private static class Item {
-        public static final Item EMPTY = new Item(0, "minecraft:air", null, null, null);
+        public static final Item EMPTY = new Item(0, "minecraft:empty", null, null, null, null);
 
         int legacyId;
         String id;
         Integer damage;
         Integer count;
         String nbt_b64;
+        Integer blockState;
     }
 
     @Value
     private static class Recipes {
         int version;
+        ArrayNode palette;
         List<CraftingDataEntry> recipes;
         List<PotionMixDataEntry> potionMixes;
         List<ContainerMixDataEntry> containerMixes;
